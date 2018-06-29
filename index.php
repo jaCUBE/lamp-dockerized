125b85a9673a6e3fca5f7a73cb72845e082b87c1
jaCUBE <jacube.cz@gmail.com>
2018-06-29 23:41:39 +0200

    Added Redic and ElasticSearch, connected within a network

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<?php phpinfo();
+<?php
+
+
+// phpinfo();
+
+
+// Example PDO connection to MariaDB container
+// $myPDO = new PDO('mysql:host=app-mariadb;dbname=test', 'root', '12345');
+
+// Example Redis connection to Redis container
+// $redis = new Redis();
+// $redis->connect('app-redis', 6379);
